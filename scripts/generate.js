5a47dfceb604397e717b0f4028654c0adafcec9d
Shubham Sharma <sharmashibu787@gmail.com>
2026-04-13 00:38:17 +0530

    updated

--- scripts/generate.js
+++ scripts/generate.js
@@ -4,10 +4,12 @@ if (!fs.existsSync("out")) {
   fs.mkdirSync("out");
 }
 
+// simple valid placeholder GIF (1x1 pixel)
 const gif = Buffer.from(
-  "GIF89a\u0001\u0000\u0001\u0000\u00ff\u0000\u0000\u0000\u0000\u0000!\xf9\u0004\u0001\u0000\u0000\u0000\u0000,\u0000\u0000\u0000\u0000\u0001\u0000\u0001\u0000\u0000\u0002\u0002D\u0001\u0000;"
+  "R0lGODlhAQABAIAAAAUEBA==",
+  "base64"
 );
 
 fs.writeFileSync("out/readme.gif", gif);
 
-console.log("GIF generated");
+console.log("Valid GIF generated");
