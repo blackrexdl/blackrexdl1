a69a9d2c4caab85b8cc0bdfbd6566e50cbe576bc
Shubham Sharma <sharmashibu787@gmail.com>
2026-04-13 00:31:08 +0530

    updated

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,19 +1,13 @@
 const fs = require("fs");
 
-const username = "blackrexdl";
-
-// simple placeholder generator logic
-// (real version can use GitHub API later)
-
 if (!fs.existsSync("out")) {
   fs.mkdirSync("out");
 }
 
-// fake GIF placeholder (replace later with real renderer)
-const dummyGif = Buffer.from(
+const gif = Buffer.from(
   "GIF89a\u0001\u0000\u0001\u0000\u00ff\u0000\u0000\u0000\u0000\u0000!\xf9\u0004\u0001\u0000\u0000\u0000\u0000,\u0000\u0000\u0000\u0000\u0001\u0000\u0001\u0000\u0000\u0002\u0002D\u0001\u0000;"
 );
 
-fs.writeFileSync("out/readme.gif", dummyGif);
+fs.writeFileSync("out/readme.gif", gif);
 
-console.log("GIF generated for:", username);
+console.log("GIF generated");
